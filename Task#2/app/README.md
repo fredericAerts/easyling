a8df9564ce377980d8c804b091694ef915fd672e
Frederic Aerts <info@fredericaerts.com>
2017-06-02 15:33:11 +0200

    init dart console-full app

---
+++ Task#2/app/README.md
@@ -0,0 +1,3 @@
+# app
+
+A sample command-line application.
---
+++ Task#2/app/bin/main.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Frederic Aerts. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:app/app.dart' as app;
+
+main(List<String> arguments) {
+  print('Hello world: ${app.calculate()}!');
+}
